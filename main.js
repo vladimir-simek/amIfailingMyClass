d51a7f0378ffdada641724673e845b77197e4d3a
vladimir-simek <minervladik@gmail.com>
2021-04-05 20:34:52 +0200

    frontend done

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-let onLoad = function () {
-    let div = document.createElement(div)
-    div.innerHTML = document.write(<div></div>)
+let num = 0
+let generateNewCol = function () {
+    let div = document.createElement("div")
+    div.className = "row"
+    div.id = "row" + num;
+    let wtf = document.createTextNode("")
+    div.appendChild(wtf)
+    document.getElementById("rowsDiv").appendChild(div)
+
+    let innerDiv = document.createElement("div")
+    innerDiv.className = "realRow"
+    innerDiv.id = "realRow" + num
+    innerDiv.appendChild(wtf)
+    document.getElementById("row"+num).appendChild(innerDiv);
+
+    let inputName = document.createElement("a")
+    let ftw = document.createTextNode("Input your grade and weight:")
+    inputName.appendChild(ftw)
+    document.getElementById("realRow"+num).appendChild(inputName)
+
+    let gradeInput = document.createElement("input")
+    gradeInput.type = "number"
+    gradeInput.className = "inputNumber"
+    gradeInput.id = "grade" + num
+    gradeInput.placeholder = "Grade"
+    gradeInput.appendChild(wtf)
+    document.getElementById("realRow"+num).appendChild(gradeInput)
+
+    let weightInput = document.createElement("input")
+    weightInput.type = "number"
+    weightInput.className = "inputNumber"
+    weightInput.id = "weight" + num
+    weightInput.placeholder = "Weight"
+    weightInput.appendChild(wtf)
+    document.getElementById("realRow"+num).appendChild(weightInput)
+    num++
+}
+
+let calculateGrade = function () {
+
 }
